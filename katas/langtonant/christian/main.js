e93d49b0b8cdc08be945f8bed30b2e8a3e5cd776
Christian Lopez <Christian.Lopez@fundacion-jala.org>
2022-05-31 15:32:26 -0400

    Refactor main Column, Rows and Step from Ant

--- katas/langtonant/christian/main.js
+++ katas/langtonant/christian/main.js
@@ -2,8 +2,11 @@ import { init } from './module1.js';
 import { move } from './module1.js';
 import { printMatrix } from './module1.js';
 
-init(10,10);
-move(10,10,100);
+var columns = parseInt(process.argv[2]);
+var rows = parseInt(process.argv[3]);
+var steps = parseInt(process.argv[4]);
+init(columns,rows);
+move(columns,rows,steps);
 printMatrix();
 
 
