4dadc784bf12f2f4d37d95fb2aa0e58443e71651
Christian Lopez <Christian.Lopez@fundacion-jala.org>
2022-05-31 21:57:08 -0400

    Lint Fix problems whit stilo

--- katas/langtonant/christian/main.js
+++ katas/langtonant/christian/main.js
@@ -5,8 +5,8 @@ import { printMatrix } from './module1.js';
 var columns = parseInt(process.argv[2]);
 var rows = parseInt(process.argv[3]);
 var steps = parseInt(process.argv[4]);
-init(columns,rows);
-move(columns,rows,steps);
+init(columns, rows);
+move(columns, rows, steps);
 printMatrix();
 
 
--- katas/langtonant/christian/module1.js
+++ katas/langtonant/christian/module1.js
@@ -11,94 +11,86 @@ var ANTLEFT = 3;
 
 
 //function from Chargue Visual Components
-export function init(rows,cols){
+export function init(rows, cols) {
     grid = make2DArray(rows, cols);
     x = rows / 2;
     y = cols / 2;
     dir = ANTUP;
-  }
-  
-  
-    
+}
+
+
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
+        arr[i] = new Array(rows);
         for (let j = 0; j < arr[i].length; j++) {
-          arr[i][j] = "";
+            arr[i][j] = '';
         }
     }
-      return arr;
+    return arr;
 }
 
-export function printMatrix(){
-  for (let i = 0; i < grid.length; i++) {
-    var auxiliar ="";
-    for (let j = 0; j < grid[i].length; j++) {
-      auxiliar += grid[i][j] + " ";
+export function printMatrix() {
+    for (let i = 0; i < grid.length; i++) {
+        var auxiliar = '';
+        for (let j = 0; j < grid[i].length; j++) {
+            auxiliar += grid[i][j] + ' ';
+        }
+        console.log(auxiliar);
     }
-    console.log(auxiliar);
-  }
-
-  
-
 }
-  
+
 //function for Rotate to Right
 function turnRight() {
     dir++;
     if (dir > ANTLEFT) {
-      dir = ANTUP;
+        dir = ANTUP;
     }
 }
 //function for Rotate to Left
 function turnLeft() {
     dir--;
     if (dir < ANTUP) {
-      dir = ANTLEFT;
+        dir = ANTLEFT;
     }
 }
-  
-  
+
+
 //function for Move to Forward
 function moveForward(rows, cols) {
     if (dir == ANTUP) {
-      y--;
+        y--;
     } else if (dir == ANTRIGHT) {
-      x++;
+        x++;
     } else if (dir == ANTDOWN) {
-      y++;
+        y++;
     } else if (dir == ANTLEFT) {
-      x--;
+        x--;
     }
-  
+
     if (x > rows - 1) {
-      x = 0;
+        x = 0;
     } else if (x < 0) {
-      x = rows - 1;
+        x = rows - 1;
     }
     if (y > cols - 1) {
-      y = 0;
+        y = 0;
     } else if (y < 0) {
-      y = cols - 1;
+        y = cols - 1;
     }
 }
-  
+
 //Function From Defect that it function principal is Draw in the Canvas
-export function move(rows,cols,steps) {
+export function move(rows, cols, steps) {
     for (let n = 0; n < steps; n++) {
-     
-      let state = grid[x][y];
-      if (state == 0) {
-        turnRight();
-        grid[x][y] = 1;
-      } else if (state == 1) {
-        turnLeft();
-        grid[x][y] = "";
-      }
-      moveForward(rows,cols);
-  
-   }
-  
+        let state = grid[x][y];
+        if (state == 0) {
+            turnRight();
+            grid[x][y] = 1;
+        } else if (state == 1) {
+            turnLeft();
+            grid[x][y] = '';
+        }
+        moveForward(rows, cols);
+    }
 }
-  
