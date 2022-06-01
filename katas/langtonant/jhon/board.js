e92bb4aac53b8e6a8c9b786a917c77ad978da641
Jhon Ponce <Jhon.Ponce@fundacion-jala.org>
2022-05-31 20:14:13 -0400

    Code fixed with with npm lint

--- katas/langtonant/jhon/board.js
+++ katas/langtonant/jhon/board.js
@@ -1,130 +1,128 @@
 export class Board {
-    constructor(sizeX,sizeY){
-        this.positionX = sizeX/2;
-        this.positionY = sizeY/2;
-        const y = sizeY*1;
-        const x = sizeX*1;
-        console.log(x,y);
+    constructor(sizeX, sizeY) {
+        this.positionX = sizeX / 2;
+        this.positionY = sizeY / 2;
+        const y = sizeY * 1;
+        const x = sizeX * 1;
+        console.log(x, y);
         this.board = new Array(x);
         this.orientation = 0;
-        this.createBoard(x,y);
-    }
-    
-    createBoard(sizeX , sizeY){
-        const x = sizeX*1;
-        const y = sizeY*1;
-        for (var i = 0; i < x; i++) {
-        this.board[i] = new Array(y);
-        }
-        this.fillboard(x,y);
+        this.createBoard(x, y);
     }
 
-    fillboard(sizeX ,sizeY){
-        const x = sizeX*1;
-        const y = sizeY*1;
-        for(var i = 0;i<x;i++){
-            for(var j = 0;j<y;j++){
-            this.board[i][j] = ' ';
+    createBoard(sizeX, sizeY) {
+        const x = sizeX * 1;
+        const y = sizeY * 1;
+        for (var i = 0; i < x; i++) {
+            this.board[i] = new Array(y);
+        }
+        this.fillboard(x, y);
+    }
+
+    fillboard(sizeX, sizeY) {
+        const x = sizeX * 1;
+        const y = sizeY * 1;
+        for (var i = 0;i < x;i++) {
+            for (var j = 0;j < y;j++) {
+                this.board[i][j] = ' ';
             }
         }
     }
-    //aqui hubo un cambio
-    printBoard(){
-        
+
+    printBoard() {
         console.clear();
         console.log(this.board.join('\n') + '\n\n');
     }
+
     // Inicio de las iteraciones para ir actualizando la matriz
-    langtonantFunction(moves){
-        const steps = moves*1;
-        if(steps > 0){
-        var count = 0;
-        while(count<steps){
-            var x = this.positionX;
-            var y = this.positionY;
-            console.log("-----------------------------------------");
-            if(this.isPosValid(x, y)){
-                if(this.isWhiteSlot(x, y)){
-                    this.board[x][y] = '0';
-                    this.turnRight();
-                    this.updatePos();
-                    this.printBoard();
-                    count++;
-                    
-                }else{
-                    this.board[x][y] = ' ';
-                    this.turnLeft();                  
-                    this.updatePos();
-                    this.printBoard();
-                    count++;
+    langtonantFunction(moves) {
+        const steps = moves * 1;
+        if (steps > 0) {
+            var count = 0;
+            while (count < steps) {
+                var x = this.positionX;
+                var y = this.positionY;
+                console.log('-----------------------------------------');
+                if (this.isPosValid(x, y)) {
+                    if (this.isWhiteSlot(x, y)) {
+                        this.board[x][y] = '0';
+                        this.turnRight();
+                        this.updatePos();
+                        this.printBoard();
+                        count++;
+                    } else {
+                        this.board[x][y] = ' ';
+                        this.turnLeft();
+                        this.updatePos();
+                        this.printBoard();
+                        count++;
+                    }
+                } else {
+                    this.setValidPos(x, y);
                 }
             }
-            else{
-                this.setValidPos(x, y);
-            }
-        }
-        }else{
-            console.log("Insert a valid number of steps");
+        } else {
+            console.log('Insert a valid number of steps');
         }
     }
 
-    isPosValid(posXin, posYin){
-        var posX= posXin*1;
-        var posY = posYin*1;
+    isPosValid(posXin, posYin) {
+        var posX = posXin * 1;
+        var posY = posYin * 1;
         var res = Boolean(false);
-        if(posX >= 0 && posX < this.board.length){
-            if(posY >= 0 && posY < this.board.length){
+        if (posX >= 0 && posX < this.board.length) {
+            if (posY >= 0 && posY < this.board.length) {
                 res = Boolean(true);
             }
         }
         return res;
     }
-    
-    setValidPos(posXin, posYin){
-        var posX= posXin*1;
-        var posY = posYin*1;
-        if(posX < 0){
-            this.positionX = this.board.length-1;
-        }else if(posX >= this.board.length){
+
+    setValidPos(posXin, posYin) {
+        var posX = posXin * 1;
+        var posY = posYin * 1;
+        if (posX < 0) {
+            this.positionX = this.board.length - 1;
+        } else if (posX >= this.board.length) {
             this.positionX = 0;
         }
-        if(posY < 0){
-            this.positionY = this.board.length-1;
-        }else if(posY >= this.board.length){
+        if (posY < 0) {
+            this.positionY = this.board.length - 1;
+        } else if (posY >= this.board.length) {
             this.positionY = 0;
         }
     }
 
-    isWhiteSlot(posXin, posYin){
-        var posX = posXin*1;
-        var posY = posYin*1;
+    isWhiteSlot(posXin, posYin) {
+        var posX = posXin * 1;
+        var posY = posYin * 1;
         var res = Boolean(false);
-        if(this.board[posX][posY] == ' '){
+        if (this.board[posX][posY] == ' ') {
             res = Boolean(true);
         }
         return res;
     }
 
-    setColor(posXin, posYin, color){
-        var posX = posXin*1;
-        var posY = posYin*1;
+    setColor(posXin, posYin, color) {
+        var posX = posXin * 1;
+        var posY = posYin * 1;
         this.board[posX][posY] = color;
     }
 
     turnLeft() {
         this.orientation--;
         if (this.orientation < 0) {
-          this.orientation = 3;
+            this.orientation = 3;
         }
-      }
-      
+    }
+
     turnRight() {
         this.orientation++;
         if (this.orientation > 3) {
-                this.orientation = 0;    
+            this.orientation = 0;
         }
-      }
-      
+    }
+
     updatePos() {
         if (this.orientation === 0) {
             this.positionX--;
@@ -136,5 +134,4 @@ export class Board {
             this.positionY--;
         }
     }
-
 }
--- katas/langtonant/jhon/main.js
+++ katas/langtonant/jhon/main.js
@@ -1,4 +1,4 @@
- import {Board} from './board.js';
-    let boardMain = new Board(process.argv[2], process.argv[3]);
-    boardMain.printBoard();
-    boardMain.langtonantFunction(process.argv[4]);
+import {Board} from './board.js';
+let boardMain = new Board(process.argv[2], process.argv[3]);
+boardMain.printBoard();
+boardMain.langtonantFunction(process.argv[4]);
