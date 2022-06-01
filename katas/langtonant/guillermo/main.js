95d7f3d711d1526c4feba3007f145b5428af30a4
Guillermo Avila <guillermo.avila@fundacion-jala.org>
2022-05-31 19:47:46 -0500

    Added graphic part to module1js and Fix all Lint issues

--- katas/langtonant/guillermo/main.js
+++ katas/langtonant/guillermo/main.js
@@ -4,4 +4,4 @@ import { mover } from './module1.js';
 var hormiga = new ant();
 var tamano = parseInt(process.argv[2]);
 var pasos = parseInt(process.argv[3]);
-mover(hormiga,tamano,pasos);
+mover(hormiga, tamano, pasos);
