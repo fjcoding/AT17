be4466449791612124a4720870cf50abd71a9f0f
Alfredo Pereira <Alfredo.Pereira@fundacion-jala.org>
2022-05-31 20:11:00 -0400

    Fix issues With lint

--- katas/langtonant/alfredo/main.js
+++ katas/langtonant/alfredo/main.js
@@ -1,22 +1,20 @@
 import { class1 } from './module1.js';
 
 let cl = new class1();
-cl.matriz_define();
+cl.matrizdefine();
 
 let itera = process.argv[2];
 console.log(itera);
 
-while(itera-->=0)
-{
-   let antposi = cl.getAntPosition();
-   let block = cl.getmatriz()[antposi[0]][antposi[1]];
-   cl.waysmatriz(cl.getAntPosition());
-   cl.printwaymatriz()
-   cl.change_direction(block);
-   cl.change_block();
-   cl.change_positionAnt();
-   console.log("");
+while (itera-- >= 0) {
+    let antposi = cl.getAntPosition();
+    let block = cl.getmatriz()[antposi[0]][antposi[1]];
+    cl.waysmatriz(cl.getAntPosition());
+    cl.printwaymatriz();
+    cl.changedirection(block);
+    cl.changeblock();
+    cl.changepositionAnt();
+    console.log('');
 }
-
 
 
--- katas/langtonant/alfredo/module1.js
+++ katas/langtonant/alfredo/module1.js
@@ -1,130 +1,122 @@
 export class class1 {
     constructor() {
-        this.variableA = 19;
-        this.variableB = 'a string';
         this.direction = 1 ;
         this.matriz = new Array(10);
-        this.posi =new Array(2);
+        this.posi = new Array(2);
         this.posi[0] = 5;
         this.posi[1] = 5;
         this.waymatriz = new Array(10);
     }
-    change_block()
-    {
-       this.posicion= this.getAntPosition();
-       if(this.matriz[this.posicion[0]][this.posicion[1]])
-         this.matriz[this.posicion[0]][this.posicion[1]] = false;
-        else
-        this.matriz[this.posicion[0]][this.posicion[1]] = true;
+
+    changeblock() {
+        this.posicion = this.getAntPosition();
+        if (this.matriz[this.posicion[0]][this.posicion[1]]) {
+            this.matriz[this.posicion[0]][this.posicion[1]] = false;
+        } else {
+            this.matriz[this.posicion[0]][this.posicion[1]] = true;
+        }
     }
-    matriz_define()
-    {
-        for(let i=0; i<10; i++)
-            {
-                this.matriz[i]=new Array(10);
-                this.waymatriz[i] = new Array(10);
+
+    matrizdefine() {
+        for (let i = 0; i < 10 ;i++) {
+            this.matriz[i] = new Array(10);
+            this.waymatriz[i] = new Array(10);
+        }
+        for (let i = 0; i < 10 ;i++) {
+            for (let j = 0; j < 10 ;j++) {
+                this.matriz[i][j] = true;
+                this.waymatriz[i][j] = true;
             }
-        for(let i=0; i<10;i++)
-            for(let j=0; j<10;j++)
-                {
-                    this.matriz[i][j] = true;
-                    this.waymatriz[i][j] = true;
-                }
+        }
     }
-    getmatriz(){
+
+    getmatriz() {
         return this.matriz;
     }
-    change_direction(vardir) {
-            // 1->up 2->right 3->down 4->left
-        if(vardir)
-        {
-            if(this.direction != 4)
-                this.direction++;
-            else
-                this.direction=1;
-        }
-        else
-        {
-            if(this.direction!=1)
-                this.direction--;
-            else
-                this.direction=4;
 
+    changedirection(vardir) {
+        // 1->up 2->right 3->down 4->left
+        if (vardir) {
+            if (this.direction != 4) {
+                this.direction++;
+            } else {
+                this.direction = 1;
+            }
+        } else {
+            if (this.direction != 1) {
+                this.direction--;
+            } else {
+                this.direction = 4;
+            }
         }
         return this.getdirection();
     }
-    getdirection()
-    {
+
+    getdirection() {
         return this.direction;
     }
-    getAntPosition()
-    {
+
+    getAntPosition() {
         return this.posi;
     }
-    setAntPosition(newposi)
-    {  // x--> file y--> column
+
+    setAntPosition(newposi) {  // x--> file y--> column
         this.posi[0] = newposi[0];
         this.posi[1] = newposi[1];
-
     }
-    change_positionAnt()
-    {
+
+    changepositionAnt() {
         this.posicion = this.getAntPosition();
-        if(this.direction == 1)
-            {
-                this.posicion[0]=this.posicion[0]-1;
-                this.setAntPosition(this.posicion);
-
-            }
-        if(this.direction == 3)
-            {
-                this.posicion[0]=this.posicion[0]+1;
-                this.setAntPosition(this.posicion);
-
-            }
-        if(this.direction == 2)
-            {
-                this.posicion[1]=this.posicion[1]+1;
-                this.setAntPosition(this.posicion);
-
-            }
-        if(this.direction == 4)
-            {
-                this.posicion[1]=this.posicion[1]-1;
-                this.setAntPosition(this.posicion);
-
-            }
+        if (this.direction == 1) {
+            this.posicion[0] = this.posicion[0] - 1;
+            this.setAntPosition(this.posicion);
+        }
+        if (this.direction == 3) {
+            this.posicion[0] = this.posicion[0] + 1;
+            this.setAntPosition(this.posicion);
+        }
+        if (this.direction == 2) {
+            this.posicion[1] = this.posicion[1] + 1;
+            this.setAntPosition(this.posicion);
+        }
+        if (this.direction == 4) {
+            this.posicion[1] = this.posicion[1] - 1;
+            this.setAntPosition(this.posicion);
+        }
     }
-    printmatriz()
-    {   let cadena="";
-        for(let i = 0; i<10 ; i++)
-           { for(let j = 0; j<10 ; j++)
-               { if(this.matriz[i][j])
-                    cadena =cadena + "■";
-                 else
-                    cadena=cadena +" "   
-               }
-                console.log(cadena);
-                cadena ="";
-           }
 
+    printmatriz() {
+        let cadena = '';
+        for (let i = 0; i < 10 ; i++) {
+            for (let j = 0; j < 10 ; j++) {
+                if (this.matriz[i][j]) {
+                    cadena = cadena + '■';
+                } else {
+                    cadena = cadena + ' ';
+                }
+            }
+            console.log(cadena);
+            cadena = '';
+        }
     }
-    waysmatriz(position){
 
+    waysmatriz(position) {
         this.waymatriz[position[0]][position[1]] = false;
     }
-    printwaymatriz()
-    {   let cadena="";
-        for(let i = 0; i<10 ; i++)
-           { for(let j = 0; j<10 ; j++)
-               { if(this.waymatriz[i][j])
-                    cadena =cadena + "■";
-                 else
-                    cadena=cadena +" "   
-               }
-                console.log(cadena);
-                cadena ="";
-           }
+
+    printwaymatriz() {
+        let cadena = '';
+        for (let i = 0; i < 10 ; i++) {
+            for (let j = 0; j < 10 ; j++) {
+                if (this.waymatriz[i][j]) {
+                    cadena = cadena + '■';
+                } else {
+                    cadena = cadena + ' ';
+                }
+            }
+            console.log(cadena);
+            cadena = '';
+        }
     }
 }
 
