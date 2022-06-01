b48823b91d28d63c0cb3bcaf7b7923bb1a771a14
MatiasVerdugo <matias.verdugo@fundacion-jala.org>
2022-06-01 20:58:17 -0300

    ant passed by lint and fixed

---
+++ katas/langtonant/matias/module1.js
@@ -0,0 +1,14 @@
+export class class1 {
+    constructor() {
+        this.variableA = 1;
+        this.variableB = 'a string';
+    }
+
+    func1() {
+        return this.variableA;
+    }
+}
+
+export function testFunc() {
+    console.log('Hello from test func');
+}
