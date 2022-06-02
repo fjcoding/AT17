7eedf93a9516cc7a752798a0cd3e4d05719e6d41
Jorge A Ramirez <Jorge.Ramirez@fundacion-jala.org>
2022-06-01 22:27:33 -0500

    Jorge's Langtons ant after npm run lint:fix

--- katas/langtonant/jorge/jlangtonsant.js
+++ katas/langtonant/jorge/jlangtonsant.js
@@ -10,83 +10,83 @@ let ANTDOWN = 2;
 let ANTLEFT = 3;
 
 export function setup(numRow, numCol) {
-  grid = make2DArray(numRow, numCol);
-  x = numRow / 2;
-  y = numCol / 2;
-  antDir = ANTUP;
+    grid = make2DArray(numRow, numCol);
+    x = numRow / 2;
+    y = numCol / 2;
+    antDir = ANTUP;
 }
 
 function make2DArray(numCol, numRow) {
-  let arr = new Array(numCol);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(numRow);
-    for (let j = 0; j < arr[i].length; j++) {
-      arr[i][j] = 0;
+    let arr = new Array(numCol);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(numRow);
+        for (let j = 0; j < arr[i].length; j++) {
+            arr[i][j] = 0;
+        }
     }
-  }
-  return arr;
+    return arr;
 }
 
-export function printGrid(){
-  for (let i = 0; i < grid.length; i++) {
-    var fill = '0';
-    for (let j = 0; j < grid[i].length; j++) {
-        fill += grid[i][j] + '0';
+export function printGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        var fill = '0';
+        for (let j = 0; j < grid[i].length; j++) {
+            fill += grid[i][j] + '0';
+        }
+        console.log(fill);
     }
-    console.log(fill);
-}
 }
 
- function turnRight() {
-   antDir++;
-        if (antDir > ANTLEFT) {
-          antDir = ANTUP;
-        }
-      }
-      
-      function turnLeft() {
-        antDir--;
-        if (antDir < ANTUP) {
-          antDir = ANTLEFT;
-        }
-      }
-      
-      function moveForward() {
-        if (antDir == ANTUP) {
-          y--;
-        } else if (antDir == ANTRIGHT) {
-          x++;
-        } else if (antDir == ANTDOWN) {
-          y++;
-        } else if (antDir == ANTLEFT) {
-          x--;
-        }
-      
-        let numRow;
-        let numCol;
+function turnRight() {
+    antDir++;
+    if (antDir > ANTLEFT) {
+        antDir = ANTUP;
+    }
+}
 
-        if (x > numRow - 1) {
-          x = 0;
-        } else if (x < 0) {
-          x = numRow - 1;
+function turnLeft() {
+    antDir--;
+    if (antDir < ANTUP) {
+        antDir = ANTLEFT;
+    }
+}
+
+function moveForward() {
+    if (antDir == ANTUP) {
+        y--;
+    } else if (antDir == ANTRIGHT) {
+        x++;
+    } else if (antDir == ANTDOWN) {
+        y++;
+    } else if (antDir == ANTLEFT) {
+        x--;
+    }
+
+    let numRow;
+    let numCol;
+
+    if (x > numRow - 1) {
+        x = 0;
+    } else if (x < 0) {
+        x = numRow - 1;
+    }
+    if (y > numCol - 1) {
+        y = 0;
+    } else if (y < 0) {
+        y = numCol - 1;
+    }
+}
+
+export function antWalk (numRow, numCol, numSteps) {
+    for (let w = 0; w < numSteps; w++) {
+        let state = grid[x][y];
+        if (state == 0) {
+            turnRight();
+            grid[x][y] = 1;
+        } else if (state == 1) {
+            turnLeft();
+            grid[x][y] = '0';
         }
-        if (y > numCol - 1) {
-          y = 0;
-        } else if (y < 0) {
-          y = numCol - 1;
-        }
-      }
-      
-      export function antWalk (numRow, numCol, numSteps) {
-        for (let w = 0; w < numSteps; w++) {
-            let state = grid[x][y];
-            if (state == 0) {
-                turnRight();
-                grid[x][y] = 1;
-            } else if (state == 1) {
-                turnLeft();
-                grid[x][y] = '0';
-            }
-            moveForward(numRow, numCol);
-        }
-      }
+        moveForward(numRow, numCol);
+    }
+}
