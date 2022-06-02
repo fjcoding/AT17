49b6d264b7d0b239752bc5b905566fd47205c425
Guillermo Avila <guillermo.avila@fundacion-jala.org>
2022-06-01 23:37:44 -0500

    The main file was updated to use the new function name langtonnt

--- katas/langtonant/guillermo/main.js
+++ katas/langtonant/guillermo/main.js
@@ -1,7 +1,7 @@
 import { ant } from './module1.js';
-import { mover } from './module1.js';
+import { langtonant } from './module1.js';
 
 var hormiga = new ant();
 var tamano = parseInt(process.argv[2]);
 var pasos = parseInt(process.argv[3]);
-mover(hormiga, tamano, pasos);
+langtonant(hormiga, tamano, pasos);
