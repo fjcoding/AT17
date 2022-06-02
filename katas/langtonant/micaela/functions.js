12af253bd263ab44509a29cd678d6f1a44ce0ace
Micaela Kler <micaela.kler@fundacion-jala.org>
2022-06-02 10:30:52 -0300

    Add changes to function test to micaela/langtonant

--- katas/langtonant/micaela/functions.js
+++ katas/langtonant/micaela/functions.js
@@ -1,20 +1,17 @@
 //Create a function to generate a Matrix
 
-export function FuncMatrix(rows, columns) 
-{
+export function FuncMatrix(rows, columns) {
     var matrix = [];
     for (var i = 0;
         i < rows;
-        i++) 
-    {
+        i++) {
         var row = [];
         for (var j = 0;
             j < columns;
-            j++) 
-        {
+            j++) {
             row.push(0);
         }
-            matrix.push(row);
+        matrix.push(row);
     }
     return matrix;
 }
@@ -22,14 +19,10 @@ export function FuncMatrix(rows, columns)
 //Create a function to change color to black or white
 //0 is white, 1 is black
 
-export function ColorBox(matrix, row, column) 
-{
-    if (matrix[row][column] == 1) 
-    {
+export function ColorBox(matrix, row, column) {
+    if (matrix[row][column] == 1) {
         matrix[row][column] = 0;
-    }
-    else 
-    {
+    } else {
         matrix[row][column] = 1;
     }
 }
@@ -44,37 +37,28 @@ export var dir =
 };
 
 //Creat a function to express the ant movements
-export function Movements(dir1, min, max) 
-{
+export function Movements(dir1, min, max) {
     var dir2;
-    if (dir1 == dir.north) 
-    {
-        do 
-        {
+    if (dir1 == dir.north) {
+        do {
             dir2 = Math.floor(Math.random() * (max - min + 1)) + min;
         }
         while (dir2 == 2);
     }
-    if (dir1 == dir.east) 
-    {
-        do 
-        {
+    if (dir1 == dir.east) {
+        do {
             dir2 = Math.floor(Math.random() * (max - min + 1)) + min;
-        } 
+        }
         while (dir2 == 3);
     }
-    if (dir1 == dir.south) 
-    {
-        do 
-        {
+    if (dir1 == dir.south) {
+        do {
             dir2 = Math.floor(Math.random() * (max - min + 1)) + min;
         }
         while (dir2 == 0);
     }
-    if (dir1 == dir.west) 
-    {
-        do 
-        {
+    if (dir1 == dir.west) {
+        do {
             dir2 = Math.floor(Math.random() * (max - min + 1)) + min;
         }
         while (dir2 == 1);
@@ -83,50 +67,30 @@ export function Movements(dir1, min, max)
 }
 
 
-export function Walk(x, y, direction, size) 
-{
+export function Walk(x, y, direction, size) {
     var max = size - 1;
-    if (direction == dir.north) 
-    {
-        if (x == 0) 
-        {
+    if (direction == dir.north) {
+        if (x == 0) {
             x = max;
-        } 
-        else 
-        {
+        } else {
             x -= 1;
         }
-    } 
-        else if (direction == dir.east) 
-    {
-        if (y == max) 
-        {
+    } else if (direction == dir.east) {
+        if (y == max) {
             y = 0;
-        } 
-        else 
-        {
+        } else {
             y += 1;
         }
-    } 
-    else if (direction == dir.south) 
-    {
-        if (x == max) 
-        {
+    } else if (direction == dir.south) {
+        if (x == max) {
             x = 0;
-        } 
-        else 
-        {
+        } else {
             x += 1;
         }
-    } 
-    else if (direction == dir.west) 
-    {
-        if (y == 0) 
-        {
+    } else if (direction == dir.west) {
+        if (y == 0) {
             y = max;
-        }
-        else 
-        {
+        } else {
             y -= 1;
         }
     }
