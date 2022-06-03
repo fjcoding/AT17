c2418b159cd7326d0d97453c8a36262d15c6079b
MatiasVerdugo <Matias.Verdugo@fundacion-jala.org>
2022-06-03 12:30:01 -0300

    new archives with jest running

--- katas/langtonant/matias/main.js
+++ katas/langtonant/matias/main.js
@@ -1,38 +1,104 @@
-class Ant {
-    constructor(x, y) {
-        this.origPosition = this.position.copy();
-        this.position = createVector(x, y);
-        this.direction = 0;
-    }
+const ANTUP = 0;
+const ANTRIGHT = 1;
+const ANTDOWN = 2;
+const ANTLEFT = 3;
 
-    turnLeft() {
-        this.direction--;
-        if (this.direction < 0) {
-            this.direction = 3;
-        }
-    }
+export class Ant {
+  x = 0;
+  y = 0;
 
-    turnRight() {
-        this.direction++;
-        if (this.direction > 3) {
-            this.direction = 0;
-        }
-    }
+  direction = ANTUP;
 
-    moveForward() {
-        if (this.direction === 0) {
-            this.position.add(0, -1);
-        }         else if (this.direction === 1) {
-            this.position.add(1, 0);
-        }         else if (this.direction === 2) {
-            this.position.add(0, 1);
-        }        else if (this.direction === 3) {
-            this.position.add(-1, 0);
-        }
+  moveForward(width, height) {
+    switch (this.direction) {
+      case ANTUP:
+        this.x = ((this.x - 1) + width) % width;
+        break;
+      case ANTRIGHT:
+        this.y = ((this.y + 1) + height) % height;
+        break;
+      case ANTDOWN:
+        this.x = ((this.x + 1) + width) % width;
+        break;
+      case ANTLEFT:
+        this.y = ((this.y - 1) + height) % height;
+        break;
     }
+  }
 
-    reset() {
-        this.position = this.origPosition.copy();
-        this.direction = 0;
+  rotateRight() {
+    this.direction = ((this.direction + 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
+  }
+
+  rotateLeft() {
+    this.direction = ((this.direction - 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
+  }
+}
+
+export class Cell {
+  alive = false;
+
+  setAlive(alive) {
+    this.alive = alive;
+  }
+
+  get isAlive() {
+    return this.alive;
+  }
+}
+
+class Grid {
+  cells = [];
+  ant;
+  height = 0;
+  width = 0;
+  moves = 0;
+
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  init() {
+    for (let x = 0; x < this.width; x++) {
+      this.cells[x] = [];
+      for (let y = 0; y < this.height; y++) {
+        const cell = new Cell();
+        this.cells[x][y] = cell;
+      }
     }
+    this.ant = new Ant();
+    this.ant.x = this.width / 2;
+    this.ant.y = this.height / 2;
+  }
+
+  move () {
+    for (let i = 0; i < 100; i++) {
+      let cell = this.cells[this.ant.x][this.ant.y];
+      if (cell.isAlive) {
+        cell.alive = false;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
+        this.ant.rotateRight();
+        this.ant.moveForward(this.width, this.height);
+      }
+      else {
+        cell.alive = true;
+        ctx.fillStyle = 'black';
+        ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
+        this.ant.rotateLeft();
+        this.ant.moveForward(this.width, this.height);
+      }
+      ctx.fillStyle = 'red';
+      ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
+      this.moves++;
+    }
+  }
+}
+
+function moveAnt(grid) {
+  grid.move();
+  ctx.stroke();
+  var moves = document.getElementById('moves');
+  moves.innerHTML = grid.moves;
 }
--- katas/langtonant/matias/main.test.js
+++ katas/langtonant/matias/main.test.js
@@ -1,6 +1,6 @@
-import { Ant } from './main.js';
+import { Cell } from './main.js';
 
-test('ant turn right should return >3', () => {
-    const objectAnt = new Ant();
-    expect(objectAnt.turnRight()).toBeGreaterThan(3);
+test('ant alive', () => {
+    const objectAnt = new Cell();
+    expect(objectAnt.setAlive()).toBeFalsy();
 });
