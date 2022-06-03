e57bd7971f4d86b0e880734f46c55e0174da344d
Micaela Kler <micaela.kler@fundacion-jala.org>
2022-06-03 10:36:07 -0300

    Correct mistakes to function test to micaela/langtonant

--- katas/langtonant/micaela/functions.test.js
+++ katas/langtonant/micaela/functions.test.js
@@ -1,21 +1,24 @@
 import { FuncMatrix} from './functions';
 import { ColorBox } from './functions';
 
-test('FuncMatrix should return a matrix');
+test('FuncMatrix should return a matrix', () => {
 const Size = 2;
 const newmatrix = FuncMatrix(Size, Size);
 expect(newmatrix).toBe[[0, 0], [0, 0]];
+});
 
-test('ColorBox function should return 0 (white) if its 1 (black)'), () => {
+test('ColorBox function should return 0 (white) if its 1 (black)', () => {
     const testmatrix = new Array();
     testmatrix[0] = new Array();
     testmatrix[1] = new Array();
-    testmatrix[0][0] = 1;
+    testmatrix[0][0] = 0;
     testmatrix[0][1] = 1;
-    testmatrix[1][0] = 1;
+    testmatrix[1][0] = 0;
     testmatrix[1][1] = 1;
 
-    const ColorBoxrResult = ColorBox(testmatrix, 0, 0);
-    expect(ColorBoxrResult[0][0]).toBe(0);
-};
+    const ColorBoxResult = ColorBox(testmatrix, 0, 1);
+    expect(ColorBoxResult).toBe[[0, 0], [0, 1]];
+    
+});
+
 
