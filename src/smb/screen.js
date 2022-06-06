bfafdc0ebd32452db37d7ff8f0697619945ca59f
Jose Guevara <jose.guevara@fundacion-jala.org>
2022-06-06 15:21:04 -0400

    Add screen capability to set elements in row/column position and print accordingly

--- src/smb/screen.js
+++ src/smb/screen.js
@@ -1,5 +1,22 @@
-const EMPTY_PIXEL = '.';
+const BLANK_PIXEL = '.';
 
+/**
+ * Class to represent a screen that displays game content
+ *
+ * Screen is composed by rows and columns, where X can be used to select each row
+ * and Y for each column, starting bottom left position as x=0 y=0
+ *
+ * Example: Say screen 5x10 (5 rows, 10 columns)
+ *
+ * . . . . . . . . . .
+ * . . . . . . . . . .
+ * . . . . A . . . . .
+ * . . . . . . . . . .
+ * . . . . . . . B . .
+ *
+ * That said, if we ask screen to return pixel in row 2, column 4, it should
+ * return 'A', as it is the content of that pixel
+ */
 export class Screen {
     constructor(rows, columns) {
         this.rows = rows;
@@ -12,19 +29,46 @@ export class Screen {
         for (let row = 0; row < this.rows; row++) {
             this.content[row] = [];
             for (let column = 0; column < this.columns; column++) {
-                this.content[row][column] = EMPTY_PIXEL;
+                this.content[row][column] = BLANK_PIXEL;
             }
         }
     }
 
+    /**
+     * Prints screen content. Given the nature of matrix structure, if we iterate
+     * directly over array[row][column] from 0 to rows/columns size, the returned
+     * string would print a 'tipped over' screen, say, for a Screen of 3 rows and 5 columns
+     * where we have an element 'A' in pos x=0 and y=0, we would expect to print:
+     *
+     *      .....
+     *      .....
+     *      A....
+     *
+     * Internally, content matrix would be constructed like this:
+     * [
+     *   ['A', '.', '.', '.', '.']
+     *   ['.', '.', '.', '.', '.']
+     *   ['.', '.', '.', '.', '.']
+     * ]
+     *
+     * In order to print it correctly, we start printing row by row starting by
+     * the last row. Check unit tests for more examples and details
+     */
     print() {
         let printed = '';
-        for (let row = 0; row < this.rows; row++) {
-            for (let column = 0; column < this.columns; column++) {
-                printed += this.content[row][column];
+        for (let posX = this.rows - 1; posX >= 0; posX--) {
+            for (let posY = 0; posY < this.columns; posY++) {
+                printed += this.content[posX][posY];
             }
             printed += '\n';
         }
         return printed;
+    }
+
+    /**
+     * Sets an element in the given row/column (posX/posY)
+     */
+    set(posX, posY, element) {
+        this.content[posX][posY] = element;
     }
 }
--- src/smb/screen.test.js
+++ src/smb/screen.test.js
@@ -1,10 +1,62 @@
 import { Screen } from './screen';
 
-test('empty screen with rows and columns', () => {
-    const screen = new Screen(3, 5);
-    const expectedPrint =
-        '.....\n' +
-        '.....\n' +
-        '.....\n';
-    expect(screen.print()).toBe(expectedPrint);
+describe('Screen should print screen with rows and columns', () => {
+    test('Empty screen', () => {
+        const expectedPrint =
+            '.....\n' +
+            '.....\n' +
+            '.....\n';
+        const screen = new Screen(3, 5);
+        expect(screen.print()).toBe(expectedPrint);
+    });
+
+    test('Screen with 1 element in the center', () => {
+        const expectedPrint =
+            '.....\n' +
+            '..A..\n' +
+            '.....\n';
+        const screen = new Screen(3, 5);
+        screen.set(1, 2, 'A');
+        expect(screen.print()).toBe(expectedPrint);
+    });
+
+    test('Screen with 1 element in the bottom left corner', () => {
+        const expectedPrint =
+            '.....\n' +
+            '.....\n' +
+            'A....\n';
+        const screen = new Screen(3, 5);
+        screen.set(0, 0, 'A');
+        expect(screen.print()).toBe(expectedPrint);
+    });
+
+    test('Screen with 1 element in the top right corner', () => {
+        const expectedPrint =
+            '....A\n' +
+            '.....\n' +
+            '.....\n';
+        const screen = new Screen(3, 5);
+        screen.set(2, 4, 'A');
+        expect(screen.print()).toBe(expectedPrint);
+    });
+
+    test('Screen with 1 element in the top left corner', () => {
+        const expectedPrint =
+            'A....\n' +
+            '.....\n' +
+            '.....\n';
+        const screen = new Screen(3, 5);
+        screen.set(2, 0, 'A');
+        expect(screen.print()).toBe(expectedPrint);
+    });
+
+    test('Screen with 1 element in the bottom right corner', () => {
+        const expectedPrint =
+            '.....\n' +
+            '.....\n' +
+            '....A\n';
+        const screen = new Screen(3, 5);
+        screen.set(0, 4, 'A');
+        expect(screen.print()).toBe(expectedPrint);
+    });
 });
