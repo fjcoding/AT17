f092579608e9fd3ec3d504663def6f93586547ef
dufforth <pablo.antonio.hh@gmail.com>
2022-06-05 22:51:36 -0400

    add args kata solution

---
+++ katas/langtonant/pablo/module1.js
@@ -0,0 +1,16 @@
+export class class1 {
+    constructor() {
+        this.variableA = 1;
+        this.variableB = 'a string';
+    }
+
+    func1(a,b) {
+        var c = parseInt(a) + parseInt(b);
+        console.log(c);
+        return this.variableA;
+    }
+}
+
+export function testFunc() {
+    console.log('Hello from test func');
+}
