4f1050963f813aea726b2205f16e480c59ae3618
Jose Guevara <jose.guevara@fundacion-jala.org>
2022-06-06 15:24:40 -0400

    Use Screen object to display test screen with 'Mario' flying in the scenario

--- src/smb/main.js
+++ src/smb/main.js
@@ -1,28 +1,17 @@
 import readline from 'readline';
 import { Screen } from './screen.js';
 
-/*const sprites = [
-    'M..............................',
-    '.M.............................',
-    '..M............................',
-    '...M...........................',
-    '....M..........................',
-    '.....M.........................',
-    '......M........................',
-    '.......M.......................',
-    '........M......................',
-    '.........M.....................',
-    '..........M....................',
-    '...........M...................',
-];
-let index = 0;*/
+let posX = 0;
+let posY = 0;
 
 function run() {
     readline.cursorTo(process.stdout, 0, 0);
-    const screen = new Screen(50, 50);
+    const screen = new Screen(50, 100);
+    screen.set(posX, posY, 'M');
     process.stdout.write(screen.print());
-    //index = index == 11 ? 0 : index + 1;
+    posX = posX == 49 ? 0 : posX + 1;
+    posY = posY == 99 ? 0 : posY + 1;
 }
 
 console.clear();
-setInterval(run, 300);
+setInterval(run, 500);
