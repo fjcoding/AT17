a3079fc88abf0a5c9ad7476b002e805926c434aa
Jhon Ponce <Jhon.Ponce@fundacion-jala.org>
2022-06-06 20:38:49 -0400

    Fixed main and parser .js with lint

--- katas/args/jhon/main.js
+++ katas/args/jhon/main.js
@@ -1,4 +1,4 @@
-import { Parser } from "./parser.js";
+import { Parser } from './parser.js';
 
 var pars = new Parser();
 pars.parser(process.argv[2]);
--- katas/args/jhon/parser.js
+++ katas/args/jhon/parser.js
@@ -1,104 +1,101 @@
-export class Parser{
+export class Parser {
     constructor() {
-        this.args = process.argv[2]; 
+        this.args = process.argv[2];
         this.schema = {
             param0 :{
                 id: 0,
-                symbol: "l",
-                type: "boolean"
+                symbol: 'l',
+                type: 'boolean'
             },
             param1 :{
                 id: 1,
-                symbol: "p",
-                type: "int"
+                symbol: 'p',
+                type: 'int'
             },
             param2 :{
-                id: "2",
-                symbol: "d",
-                type: "String"
+                id: '2',
+                symbol: 'd',
+                type: 'String'
             }
-        }
+        };
     }
 
-    parser(args){
+    parser(args) {
         let res = {
             l : false,
             p : 0,
-            d : ""
-        }
+            d : ''
+        };
         let pos = 0;
-        while(pos < args.length){
-        let char = args.charAt(pos);
-            if(char == '-'){
-                char = args.charAt(pos = pos+1);
-                let charR = args.charAt(pos = pos+1);
-                if(this.isCommand(char) && charR == ' ' && args.charAt(pos+1) != '-'){
+        while (pos < args.length) {
+            let char = args.charAt(pos);
+            if (char == '-') {
+                char = args.charAt(pos = pos + 1);
+                let charR = args.charAt(pos = pos + 1);
+                if (this.isCommand(char) && charR == ' ' && args.charAt(pos + 1) != '-') {
                     let cont = this.getValue(args, pos);
-                        pos = pos + cont.length+2;
-                        res[char] = this.fillSchema(cont, char);
-                        console.log(res);
-                    
-                }else{
+                    pos = pos + cont.length + 2;
+                    res[char] = this.fillSchema(cont, char);
+                    console.log(res);
+                } else {
                     res[char] = this.fillSchema('', char);
                     pos++;
-                    
                 }
-            }else{
-                console.log("Inserte argumentos validos");
+            } else {
+                console.log('Inserte argumentos validos');
                 break;
             }
         }
         return res;
     }
 
-    isCommand(char){
+    isCommand(char) {
         let flag = Boolean(false);
-        for(let x = 0; x < 3; x++){
-        let args = "param" + x;
-        let charAux = this.schema[args]["symbol"];
-        if(char == charAux){
-            flag = Boolean(true);
-        }
+        for (let x = 0; x < 3; x++) {
+            let args = 'param' + x;
+            let charAux = this.schema[args]['symbol'];
+            if (char == charAux) {
+                flag = Boolean(true);
+            }
         }
         return flag;
     }
 
-    getValue(args, pos){
+    getValue(args, pos) {
         let res = '';
-        for(let x = pos+1; x < args.length; x++){
-            if(args.charAt(x) != ' '){
+        for (let x = pos + 1; x < args.length; x++) {
+            if (args.charAt(x) != ' ') {
                 res = res + args.charAt(x);
-            }else{
+            } else {
                 x = args.length;
             }
         }
         return res;
     }
 
-    fillSchema(cont, command){
+    fillSchema(cont, command) {
         let flag;
-        if(command == 'l'){ 
-            if(cont.length <= 0){ 
+        if (command == 'l') {
+            if (cont.length <= 0) {
                 flag = Boolean(true);
-            }else{
+            } else {
                 flag = Boolean(false);
             }
-        }else if(command == 'p'){
-            if(this.isNumber(cont)){
-                flag = cont*1;
-            }else{
+        } else if (command == 'p') {
+            if (this.isNumber(cont)) {
+                flag = cont * 1;
+            } else {
                 flag = 0;
             }
-        }
-        else if(command == 'd'){
+        } else if (command == 'd') {
             flag = cont;
         }
         return flag;
     }
-    isNumber(number){
+
+    isNumber(number) {
         const regex = /^[0-9]*$/;
         const flag = regex.test(number);
         return flag;
     }
-
 }
