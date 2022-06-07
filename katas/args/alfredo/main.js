24fdc626352d8aa8694d897a069c6830aa4c4f09
Alfredo Pereira <Alfredo.Pereira@fundacion-jala.org>
2022-06-06 23:51:08 -0400

    Rename file ModuleArg to ModuleOne and implemented new file Scheme

--- katas/args/alfredo/main.js
+++ katas/args/alfredo/main.js
@@ -1,29 +1,24 @@
-import { ModuleArgs } from './ModuleArgs.js';
+import { ModuleOne } from './ModuleOne.js';
 
 //npm run args:alfredo -- "-l -d /user/val -p 8080 -g world,aleatory,phase -d 5,4,-3,-2" example of use
-
 let string = process.argv[2];
 console.log('String to eval : ' + string);
-const Marg = new ModuleArgs();
-if (Marg.getP(string) == null) {
+const Marg = new ModuleOne(string);
+let resultSchema = Marg.exeProcess();
+if (!resultSchema[1].isDefine) {
     console.log('The parameter p is not present');
 } else {
-    if (Marg.getD(string) == null) {
+    if (!resultSchema[2].isDefine) {
         console.log('The parameter d is not presents');
     } else {
-        let paramL = Marg.getL(string);
-        let paramP = Marg.getP(string);
-        let paramD = Marg.getD(string);
-        let paramG = Marg.getG(string);
-        let paramDe = Marg.getDecimal(string);
-        console.log(paramL);
-        console.log(paramP);
-        console.log(paramD);
-        if (paramG != null) {
-            console.log(paramG);
+        console.log(resultSchema[0]);
+        console.log(resultSchema[1]);
+        console.log(resultSchema[2]);
+        if (resultSchema[3].isDefine) {
+            console.log(resultSchema[3]);
         }
-        if (paramDe != null) {
-            console.log(paramDe);
+        if (resultSchema[4].isDefine) {
+            console.log(resultSchema[4]);
         }
     }
 }
--- katas/args/alfredo/module1.test.js
+++ katas/args/alfredo/module1.test.js
@@ -1,40 +1,62 @@
-import { ModuleArgs } from './ModuleArgs.js';
+import { ModuleOne } from './ModuleOne.js';
+import { Schema } from './Schema.js';
 
 test('get argument -l', () => {
-    const classArgs = new ModuleArgs;
-    let test1 = classArgs.getL('-l');
-    let result = new Object();
-    result.identifier = '-l';
-    result.value = true;
-    expect(test1).toEqual(result);
+    const classArgs = new ModuleOne('-l');
+    let result = classArgs.exeProcess();
+    let varScheme = new Schema();
+    let expected = varScheme.getSchema();
+    expected[0].parameter = '-l';
+    expected[0].value = true;
+    expected[0].isDefine = true;
+    expect(expected[0]).toEqual(result[0]);
 });
 
 test('get argument -p', () => {
-    const classArgs = new ModuleArgs;
-    let test1 = classArgs.getP('-p 9091');
-    let result = new Object();
-    result.identifier = '-p';
-    result.value = 9091;
-    expect(test1).toEqual(result);
+    const classArgs = new ModuleOne('-p 9091');
+    let result = classArgs.exeProcess();
+    let varScheme = new Schema();
+    let expected = varScheme.getSchema();
+    expected[1].parameter = '-p';
+    expected[1].value = 9091;
+    expected[1].isDefine = true;
+    expect(expected[1]).toEqual(result[1]);
 });
 
 test('get argument -d', () => {
-    const classArgs = new ModuleArgs;
-    let test1 = classArgs.getD('-d /dir/user/desktop/file');
-    let result = new Object();
-    result.identifier = '-d';
-    result.value = '/dir/user/desktop/file';
-    expect(test1).toEqual(result);
+    const classArgs = new ModuleOne('-d /dir/user/desktop/file');
+    let result = classArgs.exeProcess();
+    let varScheme = new Schema();
+    let expected = varScheme.getSchema();
+    expected[2].parameter = '-d';
+    expected[2].value = '/dir/user/desktop/file';
+    expected[2].isDefine = true;
+    expect(expected[2]).toEqual(result[2]);
 });
 
 test('get argument -g', () => {
-    const classArgs = new ModuleArgs;
-    let test1 = classArgs.getG('-g room,table,wall');
-    let result = new Object();
-    result.identifier = '-g';
-    result.value = ['room', 'table', 'wall'];
-    expect(test1).toEqual(result);
+    const classArgs = new ModuleOne('-g room,table,wall');
+    let result = classArgs.exeProcess();
+    let varScheme = new Schema();
+    let expected = varScheme.getSchema();
+    expected[3].parameter = '-g';
+    expected[3].value = ['room', 'table', 'wall'];
+    expected[3].isDefine = true;
+    expect(expected[3]).toEqual(result[3]);
 });
+
+test('get argument -d', () => {
+    const classArgs = new ModuleOne('-d 1,2,3,-4,19,-800,800');
+    let result = classArgs.exeProcess();
+    let varScheme = new Schema();
+    let expected = varScheme.getSchema();
+    expected[4].parameter = '-d';
+    expected[4].value = [1, 2, 3, -4, 19, -800, 800];
+    expected[4].isDefine = true;
+    expect(expected[4]).toEqual(result[4]);
+});
+/*
+
 
 test('get argument -d list decimal', () => {
     const classArgs = new ModuleArgs;
@@ -43,4 +65,4 @@ test('get argument -d list decimal', () => {
     result.identifier = '-d';
     result.value = [1, 2, 3, -4, 19, -800, 800];
     expect(test1).toEqual(result);
-});
+});*/
