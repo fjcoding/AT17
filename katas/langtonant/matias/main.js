fed9b5243c2ab3ecc46131f44db146693ead4df2
MatiasVerdugo <matias.verdugo@fundacion-jala.org>
2022-06-06 21:52:21 -0300

    no errors on lint, or in jest, updated 06/06

--- katas/langtonant/matias/main.js
+++ katas/langtonant/matias/main.js
@@ -4,101 +4,76 @@ const ANTDOWN = 2;
 const ANTLEFT = 3;
 
 export class Ant {
-  x = 0;
-  y = 0;
+    x = 0;
 
-  direction = ANTUP;
+    y = 0;
 
-  moveForward(width, height) {
-    switch (this.direction) {
-      case ANTUP:
-        this.x = ((this.x - 1) + width) % width;
-        break;
-      case ANTRIGHT:
-        this.y = ((this.y + 1) + height) % height;
-        break;
-      case ANTDOWN:
-        this.x = ((this.x + 1) + width) % width;
-        break;
-      case ANTLEFT:
-        this.y = ((this.y - 1) + height) % height;
-        break;
+    direction = ANTUP;
+
+    moveForward(width, height) {
+        switch (this.direction) {
+        case ANTUP:
+            this.x = ((this.x - 1) + width) % width;
+            break;
+        case ANTRIGHT:
+            this.y = ((this.y + 1) + height) % height;
+            break;
+        case ANTDOWN:
+            this.x = ((this.x + 1) + width) % width;
+            break;
+        case ANTLEFT:
+            this.y = ((this.y - 1) + height) % height;
+            break;
+        }
     }
-  }
 
-  rotateRight() {
-    this.direction = ((this.direction + 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
-  }
+    rotateRight() {
+        this.direction = ((this.direction + 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
+    }
 
-  rotateLeft() {
-    this.direction = ((this.direction - 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
-  }
+    rotateLeft() {
+        this.direction = ((this.direction - 1) + (ANTLEFT + 1)) % (ANTLEFT + 1);
+    }
 }
 
 export class Cell {
-  alive = false;
+    alive = false;
 
-  setAlive(alive) {
-    this.alive = alive;
-  }
+    setAlive(alive) {
+        this.alive = alive;
+    }
 
-  get isAlive() {
-    return this.alive;
-  }
+    get isAlive() {
+        return this.alive;
+    }
 }
 
-class Grid {
-  cells = [];
-  ant;
-  height = 0;
-  width = 0;
-  moves = 0;
+export class Grid {
+    cells = [];
 
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
+    ant;
 
-  init() {
-    for (let x = 0; x < this.width; x++) {
-      this.cells[x] = [];
-      for (let y = 0; y < this.height; y++) {
-        const cell = new Cell();
-        this.cells[x][y] = cell;
-      }
+    height = 0;
+
+    width = 0;
+
+    moves = 0;
+
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
     }
-    this.ant = new Ant();
-    this.ant.x = this.width / 2;
-    this.ant.y = this.height / 2;
-  }
 
-  move () {
-    for (let i = 0; i < 100; i++) {
-      let cell = this.cells[this.ant.x][this.ant.y];
-      if (cell.isAlive) {
-        cell.alive = false;
-        ctx.fillStyle = 'white';
-        ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
-        this.ant.rotateRight();
-        this.ant.moveForward(this.width, this.height);
-      }
-      else {
-        cell.alive = true;
-        ctx.fillStyle = 'black';
-        ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
-        this.ant.rotateLeft();
-        this.ant.moveForward(this.width, this.height);
-      }
-      ctx.fillStyle = 'red';
-      ctx.fillRect(this.ant.x, this.ant.y, 1, 1);
-      this.moves++;
+    init() {
+        for (let x = 0; x < this.width; x++) {
+            this.cells[x] = [];
+            for (let y = 0; y < this.height; y++) {
+                const cell = new Cell();
+                this.cells[x][y] = cell;
+            }
+        }
+        this.ant = new Ant();
+        this.ant.x = this.width / 2;
+        this.ant.y = this.height / 2;
     }
-  }
-}
-
-function moveAnt(grid) {
-  grid.move();
-  ctx.stroke();
-  var moves = document.getElementById('moves');
-  moves.innerHTML = grid.moves;
 }
