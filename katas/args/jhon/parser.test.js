453b3b94cbeb0dc2f8b41bfb14486a8d6bbba6f5
Jhon Ponce <Jhon.Ponce@fundacion-jala.org>
2022-06-06 20:38:22 -0400

    Fixed with lint the parser.test.js file

--- katas/args/jhon/parser.test.js
+++ katas/args/jhon/parser.test.js
@@ -1,13 +1,13 @@
-import { Parser } from "./parser.js";
+import { Parser } from './parser.js';
 
 test('parser should return the argument parsed ', () => {
     let resExpected = {
         l : true,
         p : 1010,
-        d : "/path/etc"
-    }
+        d : '/path/etc'
+    };
     let parser = new Parser;
-    let valActual = parser.parser("-l -p 1010 -d /path/etc");
+    let valActual = parser.parser('-l -p 1010 -d /path/etc');
     expect(valActual).toEqual(resExpected);
 });
 
@@ -15,12 +15,12 @@ test('parser should return l:false because arg \'l\' is not in the arg or is fil
     let resExpected = {
         l : false,
         p : 1010,
-        d : "/path/etc"
-    }
+        d : '/path/etc'
+    };
     let parser = new Parser;
-    let valActual = parser.parser("-p 1010 -d /path/etc");
+    let valActual = parser.parser('-p 1010 -d /path/etc');
     expect(valActual).toEqual(resExpected);
-    let valActual2 = parser.parser("-l asdasd -p 1010 -d /path/etc");
+    let valActual2 = parser.parser('-l asdasd -p 1010 -d /path/etc');
     expect(valActual2).toEqual(resExpected);
 });
 
@@ -28,36 +28,36 @@ test('parser should return p: 0 because arg \'p\' not contain a number', () => {
     let resExpected = {
         l : true,
         p : 0,
-        d : "/path/etc"
-    }
+        d : '/path/etc'
+    };
     let parser = new Parser;
-    let valActual = parser.parser("-l -p 1010asd -d /path/etc");
+    let valActual = parser.parser('-l -p 1010asd -d /path/etc');
     expect(valActual).toEqual(resExpected);
-    let valActual2 = parser.parser("-l -p asdasdasd -d /path/etc");
+    let valActual2 = parser.parser('-l -p asdasdasd -d /path/etc');
     expect(valActual2).toEqual(resExpected);
 });
 
 
-test('iscommand should return true is exist in the schema', () => { 
-    let valActual = new Parser().isCommand("l");
+test('iscommand should return true is exist in the schema', () => {
+    let valActual = new Parser().isCommand('l');
     expect(valActual).toEqual(true);
-    valActual = new Parser().isCommand("p");
+    valActual = new Parser().isCommand('p');
     expect(valActual).toEqual(true);
-    valActual = new Parser().isCommand("d");
+    valActual = new Parser().isCommand('d');
     expect(valActual).toEqual(true);
-    valActual = new Parser().isCommand("x");
+    valActual = new Parser().isCommand('x');
     expect(valActual).toEqual(false);
 });
 
 test('getValue should return the value of te command', () => {
-    let valExpected = "lineaX";
-    let valActual = new Parser().getValue("-d lineaX asdasdasd",2);
+    let valExpected = 'lineaX';
+    let valActual = new Parser().getValue('-d lineaX asdasdasd', 2);
     expect(valActual).toEqual(valExpected);
 });
 
 test('isNumber should return the value of the command', () => {
-    let valActual = new Parser().isNumber("56029");
+    let valActual = new Parser().isNumber('56029');
     expect(valActual).toEqual(true);
-    let valActual2 = new Parser().isNumber("56029swa");
+    let valActual2 = new Parser().isNumber('56029swa');
     expect(valActual2).toEqual(false);
 });
