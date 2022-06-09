eba5976445a4f5e4a4685e99c40988a3ff70ef91
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-09 13:57:17 -0400

    modify the sceneracion init and border with test unit

---
+++ src/spaceinvaders/scenario.js
@@ -0,0 +1,81 @@
+
+
+export class Scenario{
+    constructor(row,col,content){
+        this.row = row+2;
+        this.col = col+2;
+        this.content = content;
+    }
+
+    initBoard() {
+/**
+ * This function create a vector bidimensional with borders adition but all of them will have character (' . ')
+ *
+ * . . . . 
+ * . . . .  
+ * . . . . 
+ * . . . . 
+ *
+ * return this content
+ */
+        for (let row = 0; row < this.row; row++) {
+            this.content[row] = [];
+            for (let column = 0; column < this.col; column++) {
+                this.content[row][column] = " . ";
+            }
+        }
+        return this.content;
+    }
+
+    putBorder(){
+/**
+ * This function create a border on the content with specific characters
+ *
+ * ╔-----╗
+ * | . . | 
+ * | . . | 
+ * ╚-----╝
+ *
+ * return this content
+ */
+        for (let i = 0; i < this.row; i++) {
+            this.content[0][i] = '---';
+        }
+        for (let i = 0; i < this.row; i++) {
+            this.content[this.row-1][i] = '---';
+        }
+        for (let i = 0; i < this.col; i++) {
+            this.content[i][0] = ' | ';
+        }
+        for (let i = 0; i < this.col; i++) {
+            this.content[i][this.col-1] = ' | ';
+        }
+        this.content[0][0]=' ╚-';
+        this.content[0][this.row-1]='-╝ ';
+        this.content[this.col-1][0]=' ╔-';
+        this.content[this.col-1][this.row-1]='-╗ ';
+        return this.content;
+    }
+  
+
+    print() {
+/**
+ * This function print in one chain and it will give all the positions the character send like:
+ *
+ * printed : ╔-----╗ +'\n' + | . . | '\n' + | . . | '\n' + ╚-----╝' \n'
+ * 
+ * 
+ * return all the chain to print on console
+ */
+        let printed = '';
+        for (let posX = this.row - 1; posX >= 0; posX--) {
+            for (let posY = 0; posY < this.col; posY++) {
+                printed += this.content[posX][posY];
+            }
+            printed += '\n';
+        }
+        return printed;
+    }
+
+   
+}
---
+++ src/spaceinvaders/scenario.test.js
@@ -0,0 +1,39 @@
+import { Scenario } from './scenario.js';
+
+test('the function inicialize the scenario and the content on center it will be the dimation in this case 2x2', () => {
+   
+    let row = 2;
+    let col = 2;
+    let content = []
+
+    let board = new Scenario(row,col, content);
+    board.initBoard();
+
+    expect(board.content[1][1]).toBe(' . ');
+    expect(board.content[1][2]).toBe(' . ');
+    expect(board.content[2][1]).toBe(' . ');
+    expect(board.content[2][2]).toBe(' . ');
+});
+test('the function to comprobe the border of the scenario', () => {
+   
+    let row = 2;
+    let col = 2;
+    let content = []
+ 
+    let board = new Scenario(row,col, content);
+    board.initBoard();
+    board.putBorder();
+
+    expect(board.content[0][1]).toBe('---');
+    expect(board.content[0][2]).toBe('---');
+    expect(board.content[3][1]).toBe('---');
+    expect(board.content[3][2]).toBe('---');
+    expect(board.content[1][0]).toBe(' | ');
+    expect(board.content[2][0]).toBe(' | ');
+    expect(board.content[1][3]).toBe(' | ');
+    expect(board.content[2][3]).toBe(' | ');
+    expect(board.content[0][0]).toBe(' ╚-');
+    expect(board.content[0][3]).toBe('-╝ ');
+    expect(board.content[3][0]).toBe(' ╔-');
+    expect(board.content[3][3]).toBe('-╗ ');
+});
