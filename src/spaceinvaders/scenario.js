059e54d426809d8165be721a9d17db9d845f0d17
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-10 09:16:05 -0400

    change position for the comments out of functions

--- src/spaceinvaders/scenario.js
+++ src/spaceinvaders/scenario.js
@@ -6,18 +6,18 @@ export class Scenario {
         this.col = col + 2;
         this.content = [];
     }
-
+    /**
+     * This function create a vector bidimensional with borders adition but all of them will have character (' . ')
+     *
+     * . . . .
+     * . . . .
+     * . . . .
+     * . . . .
+     *
+     * return this content
+     */
     initBoard() {
-        /**
- * This function create a vector bidimensional with borders adition but all of them will have character (' . ')
- *
- * . . . .
- * . . . .
- * . . . .
- * . . . .
- *
- * return this content
- */
+       
         for (let row = 0; row < this.row; row++) {
             this.content[row] = [];
             for (let column = 0; column < this.col; column++) {
@@ -29,18 +29,18 @@ export class Scenario {
     getBoard() {
         return this.content;
     }
-
+    /**
+     * This function create a border on the content with specific characters
+     *
+     * ╔-----╗
+     * | . . |
+     * | . . |
+     * ╚-----╝
+     *
+     * return this content
+     */
     putBorder() {
-        /**
- * This function create a border on the content with specific characters
- *
- * ╔-----╗
- * | . . |
- * | . . |
- * ╚-----╝
- *
- * return this content
- */
+        
         for (let i = 0; i < this.col; i++) {
             this.content[0][i] = '---';
         }
@@ -60,16 +60,15 @@ export class Scenario {
         return this.content;
     }
 
-
+    /**
+     * This function print in one chain and it will give all the positions the character send like:
+     *
+     * printed : ╔-----╗ +'\n' + | . . | '\n' + | . . | '\n' + ╚-----╝' \n'
+     *
+     * return all the chain to print on console
+     */
     print() {
-        /**
- * This function print in one chain and it will give all the positions the character send like:
- *
- * printed : ╔-----╗ +'\n' + | . . | '\n' + | . . | '\n' + ╚-----╝' \n'
- *
- *
- * return all the chain to print on console
- */
+        
         let printed = '';
         for (let posX = this.row - 1; posX >= 0; posX--) {
             for (let posY = 0; posY < this.col; posY++) {
