93533151a0940794f89c8449e3cd5d632e34d6f2
Maria Ines Barrios <Maria.Barrios@fundacion-jala.org>
2022-06-14 00:12:16 -0400

    random function fixed by lint

--- src/pacman/apple.js
+++ src/pacman/apple.js
@@ -1,30 +1,35 @@
 
 export class Apple {
     constructor(posX, posY) {
-        this.positionX=posX;
-        this.positionY=posY; 
-        this.notEaten=true;
-        this.id=5;
+        this.positionX = posX;
+        this.positionY = posY;
+        this.notEaten = true;
+        this.id = 5;
     }
 
     getPositionX() {
         return this.positionX;
     }
+
     getPositionY() {
         return this.positionY;
     }
+
     getnotEaten() {
         return this.notEaten;
     }
+
     setPosition(posX, posY) {
         this.positionX = posX;
         this.positionY = posY;
     }
-    random = function() {
-        let max=28;
-        let min=0;
+
+    random () {
+        let max = 28;
+        let min = 0;
         return Math.random() * (max - min) + min;
     }
+
     eaten(positionX, positionY) {
         if (positionX == this.positionX && positionY == this.positionY) {
             return true;
@@ -32,5 +37,4 @@ export class Apple {
             return false;
         }
     }
-
 }
