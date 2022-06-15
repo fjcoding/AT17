90c9a9e100f52027394b4d7bff56a1381a46a0fd
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-15 03:33:35 -0400

    fix with lint on scenario.test.js

--- src/spaceinvaders/scenario.test.js
+++ src/spaceinvaders/scenario.test.js
@@ -1,13 +1,13 @@
 import { Scenario } from './scenario.js';
 
 test('the function initialize the scenario and the content on center it will be the dimension in this case 2x2', () => {
-    let characterTest= ' . '
+    let characterTest = ' . ';
     let content = [];
-    let contentExpect=[];
-    for(let i=0;i< 4;i++){
-        contentExpect[i]=[];
-        for(let j=0;j<4;j++){
-            contentExpect[i][j]=characterTest;
+    let contentExpect = [];
+    for (let i = 0; i < 4; i++) {
+        contentExpect[i] = [];
+        for (let j = 0; j < 4; j++) {
+            contentExpect[i][j] = characterTest;
         }
     }
     let board = new Scenario(2, 2, content);
@@ -17,12 +17,12 @@ test('the function initialize the scenario and the content on center it will be 
 });
 test('the function to comprobe the border of the scenario, in this case 2x2 dimension', () => {
     let content = [];
-    let contentExpect=[
-        [' ╚-','---','---','-╝ '],
-        [' | ',' . ',' . ',' | '],
-        [' | ',' . ',' . ',' | '],
-        [' ╔-','---','---','-╗ ']
-        
+    let contentExpect = [
+        [' ╚-', '---', '---', '-╝ '],
+        [' | ', ' . ', ' . ', ' | '],
+        [' | ', ' . ', ' . ', ' | '],
+        [' ╔-', '---', '---', '-╗ ']
+
     ];
 
     let board = new Scenario(2, 2, content);
@@ -30,5 +30,4 @@ test('the function to comprobe the border of the scenario, in this case 2x2 dime
     board.putBorder();
 
     expect(board.content).toEqual(contentExpect);
-    
 });
