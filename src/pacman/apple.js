09eb4da80e0f2238a88981e15ef172243e5312a0
Maria Ines Barrios <Maria.Barrios@fundacion-jala.org>
2022-06-15 16:54:03 -0400

    apple class created and fixed by lint

---
+++ src/pacman/apple.js
@@ -0,0 +1,39 @@
+
+export class Apple {
+    constructor(posX, posY) {
+        this.positionX = posX;
+        this.positionY = posY;
+        this.notEaten = true;
+    }
+
+    getPositionX() {
+        return this.positionX;
+    }
+
+    getPositionY() {
+        return this.positionY;
+    }
+
+    getnotEaten() {
+        return this.notEaten;
+    }
+
+    setPosition(posX, posY) {
+        this.positionX = posX;
+        this.positionY = posY;
+    }
+
+    random () {
+        let max = 28;
+        let min = 0;
+        return Math.random() * (max - min) + min;
+    }
+
+    eaten(positionX, positionY) {
+        if (positionX == this.positionX && positionY == this.positionY) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
