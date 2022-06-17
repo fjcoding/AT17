dd311d679e4142bf2c3e44b26f3675f551c747c3
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-17 19:53:42 -0400

    adding function to print score and adding create space between score and lives

--- src/spaceinvaders/score.js
+++ src/spaceinvaders/score.js
@@ -1,7 +1,51 @@
-export class Score{
-
+export class Score {
     constructor() {
+        this.arrayScore = ['S', 'C', 'O', 'R', 'E', ':', ' ', '0', '0', '0', '0', '0'];
+        this.arraySpace = [];
+        this.arrayLives = ['L', 'I', 'V', 'E', 'S', ':', ' ', 'X', ' ', 'X', ' ', 'X'];
+    }
 
+    initScore(dimension) {
+        this.points = 0;
+        this.lives = 3;
+        this.arraySpace = this.putSpaceScore(dimension);
+    }
 
+    putSpaceScore(dimension) {
+        for (let size = 0; size <= dimension - (this.arrayScore.length + this.arrayLives.length); size++) {
+            this.arraySpace.push(' ');
+        }
+        return this.arraySpace;
+    }
 
-    }}
+    getPoints() {
+        return this.points;
+    }
+
+    setPoints(points) {
+        this.points = points;
+        return this.getPoints;
+    }
+
+    printScore() {
+        let printed = '';
+        for (let i = 0; i < this.arrayScore.length; i++) {
+            printed += this.arrayScore[i];
+        }
+        for (let i = 0; i < this.arraySpace.length; i++) {
+            printed += this.arraySpace[i];
+        }
+        for (let i = 0; i < this.arrayLives.length; i++) {
+            printed += this.arrayLives[i];
+        }
+        return printed;
+    }
+
+    updatePointsArray() {
+        //codigo para actualizar el array de core
+    }
+
+    updateLivesArray() {
+        //codigo para actualizar el array de vida
+    }
+}
