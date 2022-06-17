d43cbab654c995d1e0c0c3290e7372e7ac7e8636
MatiasVerdugo <matias.verdugo@fundacion-jala.org>
2022-06-17 20:33:53 -0300

    Adding score.js with lint

--- src/spaceinvaders/score.js
+++ src/spaceinvaders/score.js
@@ -1,7 +1,45 @@
-export class Score{
-
+export class Score {
     constructor() {
+        this.arrayScore = ['S', 'C', 'O', 'R', 'E', ':', ' ', '0', '0', '0', '0', '0'];
 
+        this.arrayLives = ['L', 'I', 'V', 'E', 'S', ':', ' ', 'X', ' ', 'X', ' ', 'X'];
 
+        this.arraySpace = [];
+    }
 
-    }}
+    initScore(dimension) {
+        this.points = 0;
+
+        this.lives = 3;
+
+        this.dimension = dimension;
+    }
+
+    getPoints() {
+        return this.points;
+    }
+
+    setPoints(points) {
+        this.points = points;
+
+        return this.getPoints;
+    }
+
+    printScore() {
+
+        //codigo para imprimir el score
+
+    }
+
+    updatePointsArray() {
+
+        //codigo para actualizar el array de core
+
+    }
+
+    updateLivesArray() {
+
+        //codigo para actualizar el array de vida
+
+    }
+}
