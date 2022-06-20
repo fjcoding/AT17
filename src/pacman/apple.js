3d5ad7eb5651b052e0f0f2d86a86d5ce4c43c00c
Maria Ines Barrios <Maria.Barrios@fundacion-jala.org>
2022-06-20 08:47:51 -0400

    files deleted in pacman

--- src/pacman/apple.js
+++
@@ -1,8 +0,0 @@
-import { Fruit } from "./fruit.js";
-export class Apple extends Fruit {
-    constructor(map) {
-        super(map);
-        this.icon='\x1b[93m\uD83C\uDF4E\x1b[39m';
-        this.points=700;
-    }
-}
