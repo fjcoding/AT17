e2a23c5bd6c6064a6e4d674c114a6cfa7d6f0fec
Maria Ines Barrios <Maria.Barrios@fundacion-jala.org>
2022-06-19 22:07:42 -0400

    orange, strawberry and cherry apple added and grouped in fruits file

---
+++ src/pacman/fruits/apple.js
@@ -0,0 +1,8 @@
+import { Fruit } from "./fruit.js";
+export class Apple extends Fruit {
+    constructor(map) {
+        super(map);
+        this.icon='\x1b[93m\uD83C\uDF4E\x1b[39m';
+        this.points=700;
+    }
+}
---
+++ src/pacman/fruits/strawberry.js
@@ -0,0 +1,8 @@
+import { Fruit } from "./fruit.js";
+export class Strawberry extends Fruit {
+    constructor(map) {
+        super(map);
+        this.icon='\uD83C\uDF53';
+        this.points=300;
+    }
+}
