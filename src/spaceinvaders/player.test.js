ec22ae7117382f718cb0c44deba78d76fc164051
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-20 01:37:10 -0400

    delete comments on cone in player test file also test if the all unit test working in GitHub

--- src/spaceinvaders/player.test.js
+++ src/spaceinvaders/player.test.js
@@ -116,18 +116,6 @@ test('Test getPosY returns the current value', () => {
 
     expect(player.getPosY()).toBe(1);
 });
-/*
-test('Validate function get into random movement', () => {
-    let matrixEmpty = [
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
-
-    let player = new Player(1, 1, matrixEmpty, ' W ', true);
-    player.changeDirection(true, 3, 0);
-    expect(player.flag).toBe(false);
-});
-*/
 
 test('Validate function walk rigth get into random movement', () => {
     let matrixEmpty = [
